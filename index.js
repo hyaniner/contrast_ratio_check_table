9a0dea956d27846a5e6f894e4280665ce4349952
hyaniner <hyaniner@outlook.com>
2023-10-20 00:23:38 +0900

    자바스크립트로 내용 추가하기

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log(`hya`);
+
+function add_item()
+{
+    console.log(`hya2`);
+    let main_container = document.getElementById("app_main_container");
+    let node = document.querySelector("div.color_test_item");
+    console.log(`hya3: ${node}`);
+    let new_node = node.cloneNode(true);
+    main_container.appendChild(new_node);
+}
